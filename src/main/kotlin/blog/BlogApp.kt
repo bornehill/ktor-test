f6cbd441aa721fff7b0707b9465fd8d85b1c77f9
bornehill <ali.escamilla.navarro@gmail.com>
2019-07-28 00:02:49 -0500

    Add Status Page to Errors & NotFound

--- src/main/kotlin/blog/BlogApp.kt
+++ src/main/kotlin/blog/BlogApp.kt
@@ -7,10 +7,25 @@ import io.ktor.response.*
 import io.ktor.routing.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
+import io.ktor.util.*
+import io.ktor.content.TextContent
+import java.io.*;
+import httpbin.*;
 
 fun Application.main() {
     install(DefaultHeaders)
     install(CallLogging)
+    // Here we handle unhandled exceptions from routes
+    install(StatusPages) {
+        exception<Throwable> { cause ->
+            environment.log.error(cause)
+            val error = HttpBinError(code = HttpStatusCode.InternalServerError, request = call.request.local.uri, message = cause.toString(), cause = cause)
+            call.respond(error)
+        }
+        status(HttpStatusCode.NotFound) {
+            call.respond(TextContent("Opps ${it.value} ${it.description}", ContentType.Text.Plain.withCharset(Charsets.UTF_8), it))
+        }        
+    }    
     install(Routing) {
         get("/") {
             call.respondText("My Example Blog2", ContentType.Text.Html)
